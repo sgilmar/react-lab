7c04e9160e13fd27c9bc69100ae0ce400da225e0
sgilmar <sergiogilmartinez@gmail.com>
2023-02-14 18:57:51 +0100

    reusable Twitter Card react component

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
+import {TwitterFollowCard} from "./TwitterFollowCard.jsx";
 
 export function App() {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                    className='tw-followCard-avatar'
-                    alt="El avatar de sgilmar"
-                    src="https://avatars.githubusercontent.com/u/15620043?v=4"
-                />
-                <div className='tw-followCard-info'>
-                    <strong>Sgilmar</strong>
-                    <span className='tw-followCard-infoUsername'>@armstudio_es</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            <TwitterFollowCard isFollowing userName='armstudio_es' name='Sergio Gil' />
+            <TwitterFollowCard isFollowing userName='midudev' name='Miguel Ángel Durán' />
+            <TwitterFollowCard isFollowing={false} userName='elonmusk' name='Elon Musk' />
+        </section>
     )
 }
