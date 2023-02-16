c25ccbc6c20cdaef2ed432e6135643ee9d908893
sgilmar <sergiogilmartinez@gmail.com>
2023-02-16 18:18:37 +0100

    using state with useState hook

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -2,18 +2,16 @@ import './App.css'
 import {TwitterFollowCard} from "./TwitterFollowCard.jsx";
 
 export function App() {
-    const sgilmar = {isFollowing: true, userName: 'armstudio_es'}
+
     return (
         <section className='App'>
-            <TwitterFollowCard { ...sgilmar}>
+            <TwitterFollowCard userName='armstudio_es'>
                 Sergio Gil
             </TwitterFollowCard>
-            <TwitterFollowCard isFollowing
-                               userName='midudev'>
+            <TwitterFollowCard userName='midudev'>
                 Miguel Ángel Durán
             </TwitterFollowCard>
-            <TwitterFollowCard isFollowing={false}
-                               userName='elonmusk'>
+            <TwitterFollowCard userName='elonmusk'>
                 Elon Musk
             </TwitterFollowCard>
         </section>
--- projects/00-hello-world/src/TwitterFollowCard.jsx
+++ projects/00-hello-world/src/TwitterFollowCard.jsx
@@ -1,4 +1,19 @@
-export function TwitterFollowCard({children, userName, isFollowing}) {
+import {useState} from 'react';
+
+export function TwitterFollowCard({children, userName}) {
+    // const state = useState(false)
+    // const isFollowing = state[0];
+    // const setIsFollowing = state[1];
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing) 
+    }
 
     return (
         <article className='tw-followCard'>
@@ -15,8 +30,8 @@ export function TwitterFollowCard({children, userName, isFollowing}) {
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    {buttonText}
                 </button>
             </aside>
         </article>
