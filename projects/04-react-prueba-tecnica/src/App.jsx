e928bfecc3b160a9a0e5b9c4485933a1d2d532ba
sgilmar <sergiogilmartinez@gmail.com>
2023-02-23 15:01:27 +0100

    add custom hooks for get random fact

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from 'react'
-import { getRandomFact } from './services/facts.js'
+import { useCatFact } from './hooks/useCatFact.js'
 import { useCatImage } from './hooks/useCatImage.js'
 import { CAT_PREFIX_IMAGE_URL } from './constants.js'
 import './App.css'
 
 export function App () {
-  const [fact, setFact] = useState()
+  const { fact, refreshFact } = useCatFact()
   const { imageURL } = useCatImage({ fact })
 
-// Recupera un hecho aleatorio de gatos de la primera API
-  const newRandomFact = async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
-  }
-
-  // SRP
-  useEffect(() => {
-    newRandomFact()
-  },[])
-
   const handleClick = () => {
-    newRandomFact()
+    refreshFact
   }
 
   return (
