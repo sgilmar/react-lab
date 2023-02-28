1d74754209058a5ff03cd6c145ee882a19435c18
sgilmar <sergiogilmartinez@gmail.com>
2023-02-28 13:49:42 +0100

    get the form inputs in a controlled way

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
 import {Movies} from "./components/Movies.jsx";
 import {useMovies} from "./hooks/useMovies.js";
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
   const { movies } = useMovies()
-  const inputRef = useRef()
+  const [movie, setMovie] = useState('')
+  const [error, setError] = useState(null)
+
+  // const inputRef = useRef()
 
   // get form inputs when there are many
   const handleSubmit = (event) => {
     event.preventDefault()
-    const { movie } = Object.fromEntries(
-      new window.FormData(event.target)
-    )
-    if (movie !== '') console.log(movie)
+    // const { movie } = Object.fromEntries(
+    //   new window.FormData(event.target)
+    // )
+    // if (movie !== '')
+    console.log(movie)
   }
 
   // get the form inputs when there are few
@@ -32,16 +36,48 @@ function App() {
   //   console.log(value)
   // }
 
+  const handleChange = (event) => {
+    const newMovie = event.target.value
+    if (newMovie.startsWith(' ')) return
+    setMovie(newMovie)
+  }
+
+  // Input validation
+  useEffect(() => {
+    if (movie === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (movie.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return;
+    }
+
+    if (movie.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return;
+    }
+
+    setError(null)
+  },[movie])
+
   return (
     <div className='page-movies'>
-      <h1>Buscador de Películas</h1>
-
       <header>
+        <h1>Buscador de Películas</h1>
         <form className='form' onSubmit={handleSubmit}>
           {/*<input ref={inputRef} type='text' placeholder='Avengers, Hulk, Star Wars...'/>*/}
-          <input type='text' name='movie' placeholder='Avengers, Hulk, Star Wars...'/>
+          <input
+            onChange={handleChange}
+            value={movie}
+            type='text'
+            name='movie'
+            placeholder='Avengers, Hulk, Star Wars...'
+          />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
